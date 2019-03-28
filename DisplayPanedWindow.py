e319d75330ff6db6281cf5de6241a0afbd98b68a
skr47ch <its.me.arrived@gmail.com>
2019-03-29 00:12:07 +1100

    Can now display hardcoded entries. Need to figure out how to use grid so entries are arranged.

--- DisplayPanedWindow.py
+++ DisplayPanedWindow.py
@@ -1,5 +1,4 @@
 import tkinter as tk
-from tkinter import ttk
 
 class DisplayPanedWindow(tk.Frame):
     def __init__(self, cont):
@@ -10,51 +9,53 @@ class DisplayPanedWindow(tk.Frame):
         panel.configure(sashrelief='groove', sashwidth=1.5)
         panel.pack(fill='both', expand=1)
 
-        pane_anime_status = tk.Frame(panel)
-        pane_anime_title = tk.Frame(panel)
-        pane_progress = tk.Frame(panel)
-        pane_score = tk.Frame(panel)
-        pane_type = tk.Frame(panel)
-        pane_season = tk.Frame(panel)
-        pane_end = tk.Frame(panel)
+        self.pane_anime_status = tk.Frame(panel)
+        self.pane_anime_title = tk.Frame(panel)
+        self.pane_progress = tk.Frame(panel)
+        self.pane_score = tk.Frame(panel)
+        self.pane_type = tk.Frame(panel)
+        self.pane_season = tk.Frame(panel)
+        self.pane_end = tk.Frame(panel)
 
-        panel.add(pane_anime_status)
-        panel.add(pane_anime_title)
-        panel.add(pane_progress)
-        panel.add(pane_score)
-        panel.add(pane_type)
-        panel.add(pane_season)
-        panel.add(pane_end)
+        panel.add(self.pane_anime_status)
+        panel.add(self.pane_anime_title)
+        panel.add(self.pane_progress)
+        panel.add(self.pane_score)
+        panel.add(self.pane_type)
+        panel.add(self.pane_season)
+        panel.add(self.pane_end)
 
-        panel.paneconfigure(pane_anime_status, minsize=20)
-        panel.paneconfigure(pane_anime_title, minsize=250)
-        panel.paneconfigure(pane_progress, minsize=200)
-        panel.paneconfigure(pane_score, minsize=50)
-        panel.paneconfigure(pane_type, minsize=60)
-        panel.paneconfigure(pane_season, minsize=200)
-        panel.paneconfigure(pane_end, minsize=100)
+        panel.paneconfigure(self.pane_anime_status, minsize=20)
+        panel.paneconfigure(self.pane_anime_title, minsize=250)
+        panel.paneconfigure(self.pane_progress, minsize=200)
+        panel.paneconfigure(self.pane_score, minsize=50)
+        panel.paneconfigure(self.pane_type, minsize=60)
+        panel.paneconfigure(self.pane_season, minsize=200)
+        panel.paneconfigure(self.pane_end, minsize=100)
 
-        label_anime_title = tk.Label(pane_anime_title, text='Anime title')
-        label_progress = tk.Label(pane_progress, text='Progress')
-        label_score = tk.Label(pane_score, text='Score')
-        label_type = tk.Label(pane_type, text='Type')
-        label_season = tk.Label(pane_season, text='Season')
+        self.label_anime_title = tk.Label(self.pane_anime_title, text='Anime title')
+        self.label_progress = tk.Label(self.pane_progress, text='Progress')
+        self.label_score = tk.Label(self.pane_score, text='Score')
+        self.label_type = tk.Label(self.pane_type, text='Type')
+        self.label_season = tk.Label(self.pane_season, text='Season')
 
-        label_anime_title.place(x=5)
-        label_progress.place(x=5)
-        label_score.place(x=5)
-        label_type.place(x=5)
-        label_season.place(x=5)
+        self.label_anime_title.place(x=5)
+        self.label_progress.place(x=5)
+        self.label_score.place(x=5)
+        self.label_type.place(x=5)
+        self.label_season.place(x=5)
 
-        self.add_label(pane_anime_title, 'Title')
-        self.add_label(pane_progress, 'Progress')
-        self.add_label(pane_score, 'Score')
-        self.add_label(pane_type, 'Type')
-        self.add_label(pane_season, 'Season')
 
-    def add_label(self, cont, text):
-        label = tk.Label(cont, text=f'Here goes {text}')
-        label.place(x=0, y=50)
+    def add_entry(self, pane_anime_title, pane_progress, pane_score, pane_type, pane_season, y):
+        self.add_label(self.pane_anime_title, pane_anime_title, y)
+        self.add_label(self.pane_progress, pane_progress, y)
+        self.add_label(self.pane_score, pane_score, y)
+        self.add_label(self.pane_type, pane_type, y)
+        self.add_label(self.pane_season, pane_season, y)
+
+    def add_label(self, cont, text, y1):
+        label = tk.Label(cont, text=f'{text}')
+        label.place(x=5, y=y1)
 
 if __name__ == '__main__':
     root = tk.Tk()
--- Tabs.py
+++ Tabs.py
@@ -15,29 +15,39 @@ class Tabs(tk.Frame):
 
         self.tab_currently_watching = ttk.Frame(notebook)
         notebook.add(self.tab_currently_watching,  text='Currently Watching (0)')
-        DisplayPanedWindow.DisplayPanedWindow(self.tab_currently_watching)
 
         self.tab_completed = ttk.Frame(notebook)
         notebook.add(self.tab_completed, text='Completed (0)')
-        DisplayPanedWindow.DisplayPanedWindow(self.tab_completed)
+        # DisplayPanedWindow.DisplayPanedWindow(self.tab_completed)
 
         self.tab_on_hold = ttk.Frame(notebook)
         notebook.add(self.tab_on_hold, text='On Hold (0)')
-        DisplayPanedWindow.DisplayPanedWindow(self.tab_on_hold)
+        # DisplayPanedWindow.DisplayPanedWindow(self.tab_on_hold)
 
         self.tab_dropped = ttk.Frame(notebook)
         notebook.add(self.tab_dropped, text='Dropped (0)')
-        DisplayPanedWindow.DisplayPanedWindow(self.tab_dropped)
+        # DisplayPanedWindow.DisplayPanedWindow(self.tab_dropped)
 
         self.tab_plan_to_watch = ttk.Frame(notebook)
         notebook.add(self.tab_plan_to_watch, text='Plan to Watch (0)')
-        DisplayPanedWindow.DisplayPanedWindow(self.tab_plan_to_watch)
+        # DisplayPanedWindow.DisplayPanedWindow(self.tab_plan_to_watch)
 
         notebook.place(relwidth=1, relheight=1)
+
+        d1 = DisplayPanedWindow.DisplayPanedWindow(self.tab_currently_watching)
+        d1.add_entry('Mushishi', '30/34', '10', 'Anime', 'Summer 2010', 20)
+        d1.add_entry('Mushishi', '30/34', '10', 'Anime', 'Summer 2010', 50)
+        d1.add_entry('Mushishi', '30/34', '10', 'Anime', 'Summer 2010', 90)
+
+        f1 = DisplayPanedWindow.DisplayPanedWindow(self.tab_plan_to_watch)
+        f1.add_entry('Mushishi', '30/34', '10', 'Anime', 'Summer 2010', 30)
+        f1.add_entry('Mushishi2', '30/34', '10', 'Anime', 'Summer 2010', 50)
 
 if __name__ == '__main__':
     root = tk.Tk()
     root.title(f'NiseTaiga')
     root.minsize(1000, 500)
     Tabs(root)
+
+
     root.mainloop()
