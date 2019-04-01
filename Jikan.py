809784f4c9f2b2ed486d19bf4c3bac0418df13a7
skr47ch <its.me.arrived@gmail.com>
2019-04-01 22:51:58 +1100

    Added code to overcome the limit of 300 items from MAL

--- Jikan.py
+++ Jikan.py
@@ -1,8 +1,24 @@
 from jikanpy import jikan
 
 jikan = jikan.Jikan()
+username_ = 'skr47ch'
 
-watching = jikan.user(username='skr47ch', request='animelist', argument='watching')['anime']
-completed = jikan.user(username='skr47ch', request='animelist', argument='completed')['anime']
+# profile
+anime_profile = jikan.user(username=username_, request='profile')['anime_stats']
 
-print(len(watching))
+# anime lists
+watching = []
+completed  = []
+
+page_num=1
+while len(watching) < anime_profile['watching']:
+    watching.extend(jikan.user(username=username_, request='animelist', argument='watching', page=page_num)['anime'])
+    page_num += 1
+
+page_num=1
+while len(completed) < anime_profile['completed']:
+    completed.extend(jikan.user(username=username_, request='animelist', argument='completed', page=page_num)['anime'])
+    page_num += 1
+
+# testing
+print(anime_profile['days_watched'], anime_profile['watching'], anime_profile['completed'])
